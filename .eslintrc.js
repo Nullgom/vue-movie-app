c7691efe727673a2b1363344ddf0544ac1c4f852
egovframedev <whoisy@daum.net>
2021-08-30 16:12:12 +0900

    11.영화 검색

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   // 사전에 정의된 전역 변수를 설정합니다.
   env: {
     browser: true,
-    node: true,
-    es6: true
+    node: true
   },
 
   // 사용 가능한 규칙 세트입니다.
@@ -16,7 +15,6 @@ module.exports = {
 
   // 구문 분석할 패키지를 설정합니다.(Babel, ES6^ => ES5)
   parserOptions: {
-    ecmaVersion : 6,
     parser: 'babel-eslint'
   },
 
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,27 +1,35 @@
+import axios from 'axios'
+
 export default {
-  // module!
-  namespaced: true,
-  // data!
-  state: () => ({
-    movies: []
-  }),
-  // computed!
-  getters: {
-    movieIds(state) {
-      return state.movies.map(m => m.imdbID)
-    }
-  },
+  namespaced: true, // module!
+  state: () => ({ // data!
+    movies: [],
+    message: '',
+    loading: false
+  }),  
+  getters: { }, // computed!
   // methods!
-  // 변이
-  mutations: {
+  mutations: { // 변이
+    updateState(state, payload) {
+      //  ['movies', 'message', 'loading']
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     resetMovies(state) {
       state.movies = []
     }
-  },
-  // 비동기 동작
-  actions: {
-    searchMovies() { // context.state, context.getters, context.
-      
+  },  
+  actions: { // 비동기로 동작
+    async searchMovies({ commit }, payload) {
+      const { title, type, number, year } = payload
+      const OMDB_API_KEY='7035c60c'
+
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const { Search, totalResults} = res.data
+      commit('updateState', {
+        movies: Search
+      })
     }
   }
 }
