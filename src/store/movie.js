482d552619753b812142f57529c2148d159b24e3
egovframedev <whoisy@daum.net>
2021-08-30 16:27:37 +0900

    12.영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -21,7 +21,7 @@ export default {
     }
   },  
   actions: { // 비동기로 동작
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY='7035c60c'
 
@@ -30,6 +30,23 @@ export default {
       commit('updateState', {
         movies: Search
       })
+      console.log(totalResults) // 263
+      console.log(typeof totalResults) // string
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10) // 263 / 10  -> 27
+
+      // 추가 요청!
+      if(pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > number / 10) break;
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
