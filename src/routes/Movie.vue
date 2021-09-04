381a3cd24bb366bbebe8341b3cf3aef161be4631
Codingbear <surfwon@gmail.com>
2021-09-04 20:50:04 +0900

    25.Loader

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -11,11 +11,18 @@
         <div class="skeleton etc"></div>
       </div>
     </div>
+    <Loader
+      :size="3"
+      :z-index="9"
+      :fixed="true" />
   </div>
 </template>
 
 <script>
+import Loader from '~/components/Loader'
+
 export default {
+  components: { Loader },
   created() {
     console.log(this.$route)
     this.$store.dispatch('movie/searchMovieWithId', {
