49885ff5db5a9df9b93eefe02be38678bd3946c5
Codingbear <surfwon@gmail.com>
2021-09-05 13:10:17 +0900

    36.Vuex Helpers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Logo from '~/components/Logo'
 export default {
   components: {
@@ -41,12 +42,10 @@ export default {
     }
   },
   computed: {
-    image() {
-      return this.$store.state.about.image
-    },
-    name() {
-      return this.$store.state.about.name
-    }
+    ...mapState('about', [
+      'image',
+      'name'
+    ])
   },
   methods: {
     isMatch(path) {
