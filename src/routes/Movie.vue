5bb26c8f78c3852790e74c9ed03f7017ee1c7383
Codingbear <surfwon@gmail.com>
2021-09-05 12:40:25 +0900

    34.부트스트랩 Breakpoint(반응형)

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -120,105 +120,132 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "~/scss/main";
-.container {
-  padding-top: 40px;
-}  
-.skeletons {
-  display: flex;
-  .poster {
-    flex-shrink: 0;
-    width: 500px;
-    height: 500px * 3 / 2;
-    margin-right: 70px;
-  }
-  .specs {
-    flex-grow: 1;
-  }
-  .skeleton {
-    border-radius: 10px;
-    background-color: $gray-200;
-    &.title {
-      width: 80%;
-      height: 70px;     
+  @import "~/scss/main";
+  .container {
+    padding-top: 40px;
+  }  
+  .skeletons {
+    display: flex;
+    .poster {
+      flex-shrink: 0;
+      width: 500px;
+      height: 500px * 3 / 2;
+      margin-right: 70px;
     }
-    &.spec {
-      width: 60%;
-      height: 30px;
-      margin-top: 20px;
+    .specs {
+      flex-grow: 1;
     }
-    &.plot {
-      width: 100%;
-      height: 250px;
-      margin-top: 20px;
-    }
-    &.etc {
-      width: 50%;
-      height: 50px;
-      margin-top: 20px;
-    }
-  }
-}
-.movie-details {
-  display: flex;
-  color: $gray-600;
-  .poster {
-    flex-shrink: 0;
-    width: 500px;
-    height: 500px * 3 / 2;
-    margin-right: 70px;
-    border-radius: 10px;
-    background-color: $gray-200;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-  }
-  .specs {
-    flex-grow: 1;
-    .title {
-      color: $black;
-      font-family: 'Oswald', sans-serif;
-      font-size: 70px;
-      line-height: 1;
-      margin-bottom: 30px;
-    }
-    .labels {
-      color: $primary;
-      span {
-        &::after {
-          content:"\00b7";
-          margin: 0 6px;
-        }
-        &:last-child::after {
-          display: none;
-        }
+    .skeleton {
+      border-radius: 10px;
+      background-color: $gray-200;
+      &.title {
+        width: 80%;
+        height: 70px;     
+      }
+      &.spec {
+        width: 60%;
+        height: 30px;
+        margin-top: 20px;
+      }
+      &.plot {
+        width: 100%;
+        height: 250px;
+        margin-top: 20px;
+      }
+      &.etc {
+        width: 50%;
+        height: 50px;
+        margin-top: 20px;
       }
     }
-    .plot {
-      margin-top: 20px;
+  }
+  .movie-details {
+    display: flex;
+    color: $gray-600;
+    .poster {
+      flex-shrink: 0;
+      width: 500px;
+      height: 500px * 3 / 2;
+      margin-right: 70px;
+      border-radius: 10px;
+      background-color: $gray-200;
+      background-size: cover;
+      background-position: center;
+      position: relative;
     }
-    .ratings {
-      .rating-wrap {
-        display: flex;
-        .rating {
+    .specs {
+      flex-grow: 1;
+      .title {
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 70px;
+        line-height: 1;
+        margin-bottom: 30px;
+      }
+      .labels {
+        color: $primary;
+        span {
+          &::after {
+            content:"\00b7";
+            margin: 0 6px;
+          }
+          &:last-child::after {
+            display: none;
+          }
+        }
+      }
+      .plot {
+        margin-top: 20px;
+      }
+      .ratings {
+        .rating-wrap {
           display: flex;
-          align-items: center;
-          margin-right: 32px;
-          img {
-            height: 30px;
-            flex-shrink: 0;
-            margin-right: 6px;
+          .rating {
+            display: flex;
+            align-items: center;
+            margin-right: 32px;
+            img {
+              height: 30px;
+              flex-shrink: 0;
+              margin-right: 6px;
+            }
+          }
+        }
+      }
+      h3 {
+        margin: 24px 0 6px;
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 20px;
+      }
+    }
+    @include media-breakpoint-down(xl) {
+      .poster {
+        width: 300px;
+        height: 300px * 3 / 2;
+        margin-right: 40px;
+      }
+    }
+    @include media-breakpoint-down(lg) {
+      display: block;
+      .poster {
+        margin-bottom: 40px;
+      }
+    }
+    @include media-breakpoint-down(md) {
+      .specs {
+        .title {
+          font-size: 50px;
+        }
+        .ratings {
+          .rating-wrap {
+            display: block;
+            .rating {
+              margin-top: 10px;
+            }
           }
         }
       }
     }
-    h3 {
-      margin: 24px 0 6px;
-      color: $black;
-      font-family: 'Oswald', sans-serif;
-      font-size: 20px;
-    }
   }
-
-}
 </style>
